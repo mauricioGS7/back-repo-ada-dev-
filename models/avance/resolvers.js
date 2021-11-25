8ab2bd2d9084ddfef48ff7872f2bde60609a41be
Kevin <baquerochavarro@gmail.com>
2021-11-25 15:50:35 -0500

    ajustar tipos y resolvers para no agregar si fase proyecto = TERMINADO y/o cambiar fase proyecto de NULO o INICIADO a DESARROLLO

--- models/avance/resolvers.js
+++ models/avance/resolvers.js
@@ -1,3 +1,4 @@
+import { ProjectModel } from "../proyecto/proyecto.js";
 import { ModeloAvance } from "./avance.js";
 
 const resolversAvance = {
@@ -25,17 +26,44 @@ const resolversAvance = {
   },
   Mutation: {
     crearAvance: async (parents, args) => {
-      const avanceCreado = ModeloAvance.create({
-        fechaAvance: args.fechaAvance,
-        descripcion: args.descripcion,
-        proyecto: args.proyecto,
-        creadoPor: args.creadoPor,
-        observaciones: args.observaciones,
+      const proyecto = await ProjectModel.findById({
+        _id: args.proyecto,
       });
-      return avanceCreado;
+
+      if (proyecto.fase === "TERMINADO") {
+        return null;
+      } else if (proyecto.fase === "NULO" || proyecto.fase === "INICIADO") {
+        const avanceCreado = await ModeloAvance.create({
+          fechaAvance: new Date(),
+          descripcion: args.descripcion,
+          proyecto: args.proyecto,
+          creadoPor: args.creadoPor,
+          observaciones: args.observaciones,
+        });
+
+        const proyectoEditado = await ProjectModel.findByIdAndUpdate(
+          args.proyecto,
+          {
+            fase: "DESARROLLO",
+          },
+          { new: true }
+        );
+        // console.log("proyectoEditado", proyectoEditado);
+
+        return avanceCreado;
+      } else {
+        const avanceCreado = await ModeloAvance.create({
+          fechaAvance: new Date(),
+          descripcion: args.descripcion,
+          proyecto: args.proyecto,
+          creadoPor: args.creadoPor,
+          observaciones: args.observaciones,
+        });
+        return avanceCreado;
+      }
     },
     editarAvance: async (parents, args) => {
-      const avanceEditado = ModeloAvance.findByIdAndUpdate(
+      const avanceEditado = await ModeloAvance.findByIdAndUpdate(
         args._id,
         {
           descripcion: args.descripcion,
