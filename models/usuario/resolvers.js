903a66d94a578f0688da8dc44906f7fabc63c25d
Dartrix <daniel.ftg@hotmail.com>
2021-12-03 11:24:29 -0500

    Comentarios en modelos de usuarios y autenticacion

--- models/usuario/resolvers.js
+++ models/usuario/resolvers.js
@@ -32,10 +32,13 @@ const resolversUsuario = {
 
       return usuarioCreado;
     },
+    //Muación para editar los datos de un usuario
     editarUsuario: async (parent, args) => {
+      //Busca al usuario por id el cual se manda desde el front
       const usuarioEditado = await UserModel.findByIdAndUpdate(
         args._id,
         {
+          //Si encuentra el usuario entonces puede actualziar cualquiera de los datos que se envíen desde el front
           nombre: args.nombre,
           apellido: args.apellido,
           identificacion: args.identificacion,
@@ -63,16 +66,24 @@ const resolversUsuario = {
         return usuarioEliminado;
       }
     },
+    //Muación para cambiar la contraseña
     actualizarPassword: async (parent, args) => {
+      //Encuentra el usuario que quiere cambiar la contraseña por su correo
       const usuarioEcontrado = await UserModel.findOne({ correo: args.correo });
+      //Se le pide al usuario que ingrese la contraseña actual
+      //Si coincide con la contraseña guardada en la base de datos
       if (await bcrypt.compare(args.password, usuarioEcontrado.password)) {
         const salt = await bcrypt.genSalt(10);
         console.log("Usuario encontrado");
+        //Encripta la nueva contraseña
         bcrypt.hash(args.nuevapassword, salt, async (err, hash) => {
+          //Actualiza la contraseña del usuario
           const actualizarPassword= await UserModel.findOneAndUpdate({correo: args.correo}, {password : `${hash}`},{new:true});
         });
+        //Se retorna true para que en el front le aparezca al usuario un mensaje de confirmación
         return {mensaje: true};
       }else{
+        //Si la contraseña que ingresó es erronea entonces retorna false para mostrar un mensaje de error en el front
         return{
           mensaje: false
         }
